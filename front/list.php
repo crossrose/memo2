43d8fd83df7f430a991f371cfd7ffbce10e24186
crossrose <crossrose@naver.com>
2017-04-05 17:26:03 +0900

    memo 수정

--- front/list.php
+++ front/list.php
@@ -2,19 +2,19 @@
     require_once "../modules/memo.php";
     require_once "../modules/memos.php";
     //__constructor($id,$title,$user,$passwd,$memos,$url)
-    $memo1 = new memo(1,"제목","등록자","passwd","memos","URLS");
+    $memo1 = new memo(1,"subject","reg","passwd","memos","URLS");
     $memos = new memos;
 
     $memos->add($memo1);
 
     $iterator = $memos->getIterator();
-    echo "test4";
-    print_r($iterator);
-    exit;
     foreach ($iterator as $memo) {
+
         echo $memo->getId(). " ";
         echo $memo->getTitle(). " ";
         echo $memo->getUser(). " ";
+        echo $memo->getPasswd();
+        
     }
 
 ?>
