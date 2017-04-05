96c59a252a5f98b36452443f3a74cea6fb82b3ce
crossrose <crossrose@naver.com>
2017-04-05 18:06:40 +0900

    tnwjd

--- front/list.php
+++ front/list.php
@@ -1,12 +1,9 @@
 <?php
     require_once "../modules/memo.php";
     require_once "../modules/memos.php";
+
     //__constructor($id,$title,$user,$passwd,$memos,$url)
-    $memo1 = new memo(1,"subject","reg","passwd","memos","URLS");
-    $memos = new memos;
-
-    $memos->add($memo1);
-
+    
     $iterator = $memos->getIterator();
     foreach ($iterator as $memo) {
 
@@ -14,7 +11,7 @@
         echo $memo->getTitle(). " ";
         echo $memo->getUser(). " ";
         echo $memo->getPasswd();
-        
+
     }
 
 ?>
