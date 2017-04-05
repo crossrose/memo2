50358dc58acddfeb543e6e739128d87a8341de29
crossrose <crossrose@naver.com>
2017-04-05 17:03:44 +0900

    리스트 추가

--- front/list.php
+++ front/list.php
@@ -1,5 +1,21 @@
 <?php
+    require_once "../modules/memo.php";
+    require_once "../modules/memos.php";
 
-    echo "리스트";
-    
+    //__constructor($id,$title,$user,$passwd,$memos,$url)
+    $memo1 = new memo(1,"제목","등록자","passwd","memos","URLS");
+    $memos = new memos;
+
+    $memos->add($memo1);
+
+    $iterator = $memos->getIterator();
+
+    foreach ($iterator as $memo) {
+        print $memo->getId(). " ";
+        print $memo->getTitle(). " ";
+        print $memo->getUser(). " ";
+        
+        # code...
+    }
+
 ?>
