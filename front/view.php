dd6b6a997ec9cb06085e48cd082a055272d1c0ea
crossrose <crossrose@naver.com>
2017-04-06 18:32:26 +0900

    폼 입력 추가

---
+++ front/view.php
@@ -0,0 +1,39 @@
+<?php
+ //require_once "../modules/db.php";
+
+ $database_host     = "localhost";
+ $database_name     = "db_memo";
+ $database_charset  = "utf8";
+ $database_user     = "memo_user";
+ $database_passwd   = "memo!@#$";
+
+ $dsn ="mysql:host=$database_host;dbname=$database_name;charset=$database_charset";
+ try{
+     $pdo = new PDO($dsn,$database_user,$database_passwd);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+     print ("접속하였습니다.");
+ }catch (PDOException $exception) {
+     die('오류:'.$exception->getMessage());
+
+ }
+
+ try{
+     $pdo->beginTransaction();
+     $sql = "INSERT INTO memo2 (title,name,passwd,memo,memo_link_url) value (? ,? ,? ,?,?)";
+     $shtm = $pdo->prepare($sql);
+     $shtm->bindValue(1,$_POST['memo_title'],PDO::PARAM_STR);
+     $shtm->bindValue(2,$_POST['memo_name'],PDO::PARAM_STR);
+     $shtm->bindValue(3,$_POST['memo_passwd'],PDO::PARAM_STR);
+     $shtm->bindValue(4,$_POST['memo_text'],PDO::PARAM_STR);
+     $shtm->bindValue(5,$_POST['memo_link_url'],PDO::PARMA_STR);
+     $shtm->execute();
+ }catch (PDOException $exception) {
+     $pdo->rollBack();
+     print("오류:".$exception->getMessage());
+ }
+
+
+
+
+?>
---
+++ front/write.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <title>PHP DB write Test </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+    <form name="form1" method="post" action="view.php">
+        <table>
+  			<tr>
+  				<td><label>제목</label></td>
+  				<td><input type="text" id="memo_title" name="memo_title" value="<?=$title?>"/></td>
+  			</tr>
+  			<tr>
+  				<td><label>메모내용</label></td>
+  				<td><textarea id="memo_text" name="memo_text" cols="50" rows="30"><?=$memo?></textarea></td>
+  			</tr>
+  			<tr>
+  				<td><label>작성자</label></td>
+  				<td><input type="text" id="memo_name" name="memo_name" value="<?=$name?>"/></td>
+  			</tr>
+  			<tr>
+  				<td><label>비밀번호</label></td>
+  				<td><input type="password" id="memo_passwd" name="memo_passwd" /></td>
+  			</tr>
+  			<tr>
+  				<td><label>연결 URL</label></td>
+  				<td><input type="text" id="memo_link_url" name="memo_link_url" value="<?=$memo_link_url?>"/></td>
+  			</tr>
+  			<tr>
+  				<td><input type="submit" value="<?=$button_value?>" /></td>
+  				<td><input type="button" value="취소" onclick="cancel(<?=$wherefrom?>,<?=$idx?>);" /></td>
+  			</tr>
+  		</table>
+
+    </form>
+</body>
+</html>
