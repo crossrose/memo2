d26e936052a7f8bcc5f21f3f73242d8157bf0942
crossrose <crossrose@naver.com>
2017-04-06 11:20:39 +0900

    PDO 수정

--- modules/memo.php
+++ modules/memo.php
@@ -67,8 +67,8 @@
         // 여기부터는 기능
         public function write_memo() {
             try{
-                $pdo = new PDO('mysql:host=localhost;dbname=db_memo;charset=urf8','memo_user','memo!@#$');
-                $pdo->setAttribute(PDO::ATTR_ERRMDE,PDO::ERRMODE_EXCEPTION);
+                $pdo = new PDO('mysql:host=localhost;dbname=db_memo;charset=utf8','memo_user','memo!@#$');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                 $pdo->serAttribute(PDO::ATTR_EMULATE_PREPARES,false);
                 echo "접속하였습니다.";
             }catch(PDOException $exception) {
