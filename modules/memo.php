ab07c030e03cd523012eb188026b78959d01ab82
crossrose <crossrose@naver.com>
2017-04-06 11:26:26 +0900

    수정

--- modules/memo.php
+++ modules/memo.php
@@ -66,14 +66,14 @@
 
         // 여기부터는 기능
         public function write_memo() {
-            // try{
-            //     $pdo = new PDO('mysql:host=localhost;dbname=db_memo;charset=utf8','memo_user','memo!@#$');
-            //     // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            //     // $pdo->serAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-            //     echo "접속하였습니다.";
-            // }catch(PDOException $exception) {
-            //     die('접속오류 :'.$exception->getMessage());
-            // }
+            try{
+                 $pdo = new PDO('mysql:host=localhost;dbname=db_memo;charset=utf8','memo_user','memo!@#$');
+                 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                 $pdo->serAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+                 echo "접속하였습니다.";
+                }catch(PDOException $exception) {
+                    die('접속오류 :'.$exception->getMessage());
+                }
         }
 
         public function delete_memo() {
