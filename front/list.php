d9c9b6379629c7dad492a8696bffb48fc79434c8
crossrose <crossrose@naver.com>
2017-04-10 17:26:54 +0900

    git 수정

--- front/list.php
+++ front/list.php
@@ -1,25 +1,38 @@
+<html>
+<head>
+  <title> php 테스트 </title>
+</head>
+<body>
+  <?php
+   
+   ?>
+</body>
+</html>
+
 <?php
-    require_once "../modules/memo.php";
-    require_once "../modules/memos.php";
+    // require_once "../modules/memo.php";
+    // require_once "../modules/memos.php";
+    //
+    // //__constructor($id,$title,$user,$passwd,$memos,$url)
+    // $memo1 = new memo(1,"subject","reg","passwd","memos","URLS");
+    // $memos = new memos;
+    //
+    // $memos->add($memo1);
+    //
+    // $iterator = $memos->getIterator();
+    // foreach ($iterator as $memo) {
+    //
+    //     echo $memo->getId(). " ";
+    //     echo $memo->getTitle(). " ";
+    //     echo $memo->getUser(). " ";
+    //     echo $memo->getPasswd();
+    //
+    //
+    // }
+    //
+    // $memo->write_memo();
+    //
 
-    //__constructor($id,$title,$user,$passwd,$memos,$url)
-    $memo1 = new memo(1,"subject","reg","passwd","memos","URLS");
-    $memos = new memos;
-
-    $memos->add($memo1);
-
-    $iterator = $memos->getIterator();
-    foreach ($iterator as $memo) {
-
-        echo $memo->getId(). " ";
-        echo $memo->getTitle(). " ";
-        echo $memo->getUser(). " ";
-        echo $memo->getPasswd();
-        
-
-    }
-
-    $memo->write_memo();
 
 
 ?>
